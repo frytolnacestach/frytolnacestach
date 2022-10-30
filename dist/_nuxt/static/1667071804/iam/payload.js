f422e8d1957e21f432c9070b3078c7798c063c40
frytolnacestach <frytolnacestach@gmail.com>
2022-10-30 16:13:30 +0100

    test - dist folder

---
+++ dist/_nuxt/static/1667071804/iam/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/iam", {data:[{}],fetch:{},mutations:void 0});
---
+++ dist/_nuxt/static/1667071804/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fblog","\u002F","\u002Fiam","\u002Fdonate","\u002Fcookies","\u002Fsocial","\u002Fstyleguide","\u002Fconditions"]})
