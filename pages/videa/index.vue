2ff2c44cf7c9d02e9ca4b3ad0a09e70de0cb48e1
frytolnacestach <frytolnacestach@gmail.com>
2022-11-28 18:57:48 +0100

    edit link for video

--- pages/videa/index.vue
+++ pages/videa/index.vue
@@ -13,12 +13,12 @@
                                 <div class="o-article-list__item-inner">
                                     <div class="o-article-list__image">
                                         <div class="o-article-list__image-file" v-bind:style="{ 'background-image': 'url(https://img.youtube.com/vi/' + getSlugURL(video.url) + '/0.jpg)'}">
-                                            <NuxtLink class="o-article-list__image-link" :to="`videa/${video.slug}`"></NuxtLink>
+                                            <NuxtLink class="o-article-list__image-link" :to="`/videa/${video.slug}`"></NuxtLink>
                                         </div>
                                     </div>
                                     <div class="o-article-list__text">
                                         <h3 class="o-article-list__title">
-                                            <NuxtLink class="o-article-list__title-link" :to="`videa/${video.slug}`">{{ video.title }}</NuxtLink>
+                                            <NuxtLink class="o-article-list__title-link" :to="`/videa/${video.slug}`">{{ video.title }}</NuxtLink>
                                         </h3>
                                     </div>
                                 </div>
