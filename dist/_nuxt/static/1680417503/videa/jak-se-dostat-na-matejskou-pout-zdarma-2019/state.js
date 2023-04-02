70b04145433ae3b90d1bd495a6a7b1026446054c
frytolnacestach <frytolnacestach@gmail.com>
2023-04-02 22:52:17 +0200

    Static version - Places (new data)

--- dist/_nuxt/static/1680417503/videa/jak-se-dostat-na-matejskou-pout-zdarma-2019/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1680417503",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fjak-se-dostat-na-matejskou-pout-zdarma-2019",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
