83be5ec78de3f323443b15251f9177dac3de75e0
frytolnacestach <frytolnacestach@gmail.com>
2023-04-08 10:29:52 +0200

    Search for places

--- pages/svet/index.vue
+++ pages/svet/index.vue
@@ -4,73 +4,86 @@
             <SectionHero :headline="headline" />
         </section>
 
-        <section class="t-section my-4">
+        <!-- SECTION - Vyhledávač -->
+        <section class="t-section py-4 -bg-brand3">
             <div class="t-section__inner">
-                <div class="m-headline mb-2 text-align-center">
-                    <h2 class="m-headline__title">Kontinenty</h2>
-                </div>
-                <div class="o-cover-place">
-                    <div class="o-cover-place__outer">
-                        <div class="o-cover-place__items">
-                            <div v-for="placesContinent in placesContinents" :key="placesContinent.id" class="o-cover-place__item">
-                                <div class="o-cover-place__content">
-                                    <div class="o-cover-place__image">
-                                        <div class="o-cover-place__image-file" v-bind:style="{ 'background-image': 'url(' + (placesContinent.image_cover ? placesContinent.image_cover : 'https://image.frytolnacestach.cz/storage/_default/hero.png') + ')' }"></div>
-                                    </div>
-                                    <h3 class="o-cover-place__name">
-                                        {{ placesContinent.name }}
-                                    </h3>
-                                    <NuxtLink class="o-cover-place__link" :to="`/svet/kontinent/${placesContinent.slug}`"></NuxtLink>
-                                </div>
-                            </div>
+                <div class="o-search">
+                    <div class="o-search__input">
+                        <input class="a-input-search" type="text" v-model="searchQuery" @input="filterPlaces" placeholder="Které místo tě zajímá?">
+                    </div>
+                    <span class="o-search__condition" v-if="searchQuery.length < 3">Napiš alepoň 3 znaky</span>
+                    <div class="o-search__result">
+                        <div class="m-search-result">
+                            <ul class="m-search-result__items" v-if="filteredPlaces.length > 0">
+                                <li v-for="placesSearch in filteredPlaces" :key="placesSearch.id" class="m-search-result__item">
+                                    <NuxtLink class="m-search-result__link" :to="`/svet/kontinent/${placesSearch.slug}`" v-if="placesSearch.type_place === 'continent'">
+                                        <span class="m-search-result__name">{{ placesSearch.name }}</span>
+                                        <span class="m-search-result__type">Kontinent</span>
+                                    </NuxtLink>
+
+                                    <NuxtLink class="m-search-result__link" :to="`/svet/stat/${placesSearch.slug}`" v-if="placesSearch.type_place === 'state'">
+                                        <span class="m-search-result__name">{{ placesSearch.name }}</span>
+                                        <span class="m-search-result__type">Stát</span>
+                                    </NuxtLink>
+
+                                    <NuxtLink class="m-search-result__link" :to="`/svet/mesto/${placesSearch.slug}`" v-if="placesSearch.type_place === 'city'">
+                                        <span class="m-search-result__name">{{ placesSearch.name }}</span>
+                                        <span class="m-search-result__type">Město</span>
+                                    </NuxtLink>
+                                </li>
+                            </ul>
+                            <span class="m-search-result__no-result" v-if="searchQuery.length >= 3 & filteredPlaces.length === 0">Tak tohle tu bohužel nenajdeš.. :/ <i>(zkuz to ještě jednou)</i></span>
                         </div>
                     </div>
                 </div>
             </div>
         </section>
+        <!-- SECTION - Vyhledávač END -->
 
-        <section class="t-section my-4">
+        <!-- SECTION - Typ místa -->
+        <section class="t-section py-4 -bg-brand3">
             <div class="t-section__inner">
-                <div class="m-headline mb-2 text-align-center">
-                    <h2 class="m-headline__title">Státy</h2>
-                </div>
-                <div class="o-cover-place o-cover-place--smaller">
-                    <div class="o-cover-place__outer">
-                        <div class="o-cover-place__items">
-                            <div v-for="placesState in placesStates" :key="placesState.id" class="o-cover-place__item">
-                                <div class="o-cover-place__content">
-                                    <div class="o-cover-place__image">
-                                        <div class="o-cover-place__image-file" v-bind:style="{ 'background-image': 'url(' + (placesState.image_cover ? placesState.image_cover : 'https://image.frytolnacestach.cz/storage/_default/hero.png') + ')' }"></div>
+                <div class="o-world-type">
+                    <div class="o-world-type__outer">
+                        <div class="o-world-type__inner">
+                            <div class="o-world-type__items">
+                                <div class="o-world-type__item">
+                                    <div class="o-world-type__container">
+                                        <div class="o-world-type__image">
+                                            <div class="o-world-type__image-file -continent">
+                                                <NuxtLink class="o-world-type__image-link" :to="`/svet/kontinent`"></NuxtLink>
+                                            </div>
+                                        </div>
+                                        <h2 class="o-world-type__text">
+                                            <NuxtLink class="o-world-type__link" :to="`/svet/kontinent`">KONTINENTY<span class="o-world-type__link-length"> (7 položek) </span></NuxtLink>
+                                        </h2>
                                     </div>
-                                    <h3 class="o-cover-place__name">
-                                        {{ placesState.name }}
-                                    </h3>
-                                    <NuxtLink class="o-cover-place__link" :to="`/svet/stat/${placesState.slug}`"></NuxtLink>
                                 </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
 
-        <section class="t-section my-4">
-            <div class="t-section__inner">
-                <div class="m-headline mb-2 text-align-center">
-                    <h2 class="m-headline__title">Města</h2>
-                </div>
-                <div class="o-cover-place o-cover-place--smallest">
-                    <div class="o-cover-place__outer">
-                        <div class="o-cover-place__items">
-                            <div v-for="placesCity in placesCities" :key="placesCity.id" class="o-cover-place__item">
-                                <div class="o-cover-place__content">
-                                    <div class="o-cover-place__image">
-                                        <div class="o-cover-place__image-file" v-bind:style="{ 'background-image': 'url(' + (placesCity.image_cover ? placesCity.image_cover : 'https://image.frytolnacestach.cz/storage/_default/hero.png') + ')' }"></div>
+                                <div class="o-world-type__item">
+                                    <div class="o-world-type__container">
+                                        <div class="o-world-type__image">
+                                            <div class="o-world-type__image-file -state">
+                                                <NuxtLink class="o-world-type__image-link" :to="`/svet/stat`"></NuxtLink>
+                                            </div>
+                                        </div>
+                                        <h2 class="o-world-type__text">
+                                            <NuxtLink class="o-world-type__link" :to="`/svet/stat`">STÁTY<span class="o-world-type__link-length"> (249 položek) </span></NuxtLink>
+                                        </h2>
                                     </div>
-                                    <h3 class="o-cover-place__name">
-                                        {{ placesCity.name }}
-                                    </h3>
-                                    <NuxtLink class="o-cover-place__link" :to="`/svet/mesto/${placesCity.slug}`"></NuxtLink>
+                                </div>
+
+                                <div class="o-world-type__item">
+                                    <div class="o-world-type__container">
+                                        <div class="o-world-type__image">
+                                            <div class="o-world-type__image-file -city">
+                                                <NuxtLink class="o-world-type__image-link" :to="`/svet/mesto`"></NuxtLink>
+                                            </div>
+                                        </div>
+                                        <h2 class="o-world-type__text">
+                                            <NuxtLink class="o-world-type__link" :to="`/svet/mesto`">MĚSTA<span class="o-world-type__link-length"> (2491 položek) </span></NuxtLink>
+                                        </h2>
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -93,16 +106,54 @@
             SectionPlatform
         },
 
-        methods:{
+        methods: {
             getSlugURL(url) {
                 url = url.replace("https://youtu.be/", "").replace("https://youtube.com/shorts/", "");
                 return url.replace(" ", "");
+            },
+
+            async searchPlaces() {
+                try {
+                    const [placesContinents, placesStates, placesCities] = await Promise.all([
+                        this.$axios.$get(`https://frytolnacestach-api.vercel.app/api/places-continents?search=${this.searchQuery}`),
+                        this.$axios.$get(`https://frytolnacestach-api.vercel.app/api/places-states?search=${this.searchQuery}`),
+                        this.$axios.$get(`https://frytolnacestach-api.vercel.app/api/places-cities?search=${this.searchQuery}`)
+                    ]);
+                    this.placesContinents = placesContinents;
+                    this.placesStates = placesStates;
+                    this.placesCities = placesCities;
+                } catch (error) {
+                    console.error(error)
+                }
+            },
+
+            filterPlaces() {
+                this.filteredPlaces = [];
+
+                if (!this.searchQuery) {
+                    return;
+                }
+                if (this.searchQuery.length >= 3) {
+                    this.filteredPlaces = this.placesContinents.concat(this.placesStates).concat(this.placesCities).filter(place => place.name.toLowerCase().includes(this.searchQuery.toLowerCase()));
+                }
             }
         },
 
         data() {
             return {
-                headline: "Svět"
+            headline: "Svět",
+            placesContinents: [],
+            placesStates: [],
+            placesCities: [],
+            placesSearch: '',
+            searchQuery: '',
+            filteredPlaces: []
+            }
+        },
+
+        computed: {
+            hasResults() {
+                return this.searchQuery.length >= 3 && this.filteredPlaces.length === 0;
             }
         },
 
@@ -114,14 +165,33 @@
             ]
         },
 
+        watch: {
+            searchQuery: function (newSearchQuery, oldSearchQuery) {
+                if (newSearchQuery.length == 3) {
+                    this.searchPlaces();
+                }
 
-        async asyncData({ $axios }) {
-            const [placesContinents, placesStates, placesCities] = await Promise.all([
-                $axios.$get(`https://frytolnacestach-api.vercel.app/api/places-continents`),
-                $axios.$get(`https://frytolnacestach-api.vercel.app/api/places-states`),
-                $axios.$get(`https://frytolnacestach-api.vercel.app/api/places-cities`)
-            ]);
-            return { placesContinents, placesStates, placesCities };
+                else if (newSearchQuery.substring(0, 3) !== oldSearchQuery.substring(0, 3)) {
+                    this.searchPlaces();
+                }
+
+                else if (newSearchQuery.substring(0, 3) === oldSearchQuery.substring(0, 3)) {
+                    if (filteredPlaces.length == 0 ){
+                        this.searchPlaces();
+                    }
+                } 
+                
+                else if (newSearchQuery.length >= 3) {
+                    filterPlaces()
+                }
+
+                else {
+                    this.placesContinents = [];
+                    this.placesStates = [];
+                    this.placesCities = [];
+                    this.filteredPlaces = [];
+                }
+            }
         }
     }
 </script>
