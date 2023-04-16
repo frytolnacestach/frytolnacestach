622ad215d7752afa545d3400de7b152c78e13429
frytolnacestach <frytolnacestach@gmail.com>
2023-04-16 12:23:14 +0200

    Component oBuild

---
+++ components/organisms/oBuild.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="o-build">
+        <div class="o-build__outer">
+            <div class="o-build__inner">
+                <div class="o-build__text">
+                    SEKCE SE PŘIPRAVUJE
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
--- layouts/error.vue
+++ layouts/error.vue
@@ -1,55 +1,52 @@
 <template>
-  <main class="t-main -pt-menu">
-      <section class="t-section">
-          <SectionHero 
-              :headline="headline"
-              :perex="perex"
-          />
-      </section>
+	<main class="t-main -pt-menu">
+		
+		<section class="t-section">
+			<SectionHero 
+			:headline="headline"
+			:perex="perex"
+			/>
+		</section>
 
-      <section class="t-section">
-          <div class="o-build">
-              <div class="o-build__outer">
-                  <div class="o-build__inner">
-                      <div class="o-build__text">
-                          SEKCE SE PŘIPRAVUJE
-                      </div>
-                  </div>
-              </div>
-          </div>
-      </section>
-  </main>
+		<section class="t-section">
+			<oBuild />
+		</section>
+	</main>
 </template>
 
 <script>
+import oBuild from '../components/organisms/oBuild.vue'
 import SectionHero from '../components/SectionHero.vue'
 
 export default {
+	name: 'PageNotfound',
+	layout: 'empty',
 
-  name: 'PageNotfound',
-  layout: 'empty',
-  props: {
-    error: {
-      type: Object,
-      default: null
-    }
-  },
-  components: {
-      SectionHero
-  },
-  data() {
-      return {
-          headline: "Error 404",
-          perex: "Je mi to líto ale tato stránka neexistuje"
-      }
-  },
-  head () {
-    const title =
-      this.error.statusCode === 404 ? this.pageNotFound : this.otherError
-    return {
-      title
-    }
-  }
+	components: {
+		oBuild,
+		SectionHero
+	},
+
+	props: {
+		error: {
+			type: Object,
+			default: null
+		}
+	},
+
+	data() {
+		return {
+			headline: "Error 404",
+			perex: "Je mi to líto ale tato stránka neexistuje"
+		}
+	},
+
+	head () {
+		const title = this.error.statusCode === 404 ? this.pageNotFound : this.otherError
+		return {
+			title
+		}
+	}
 }
 </script>
 
