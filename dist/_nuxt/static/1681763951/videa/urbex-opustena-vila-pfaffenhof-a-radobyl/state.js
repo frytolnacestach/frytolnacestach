f9987114862af4136fa07d603c98f9edc1b4fad6
frytolnacestach <frytolnacestach@gmail.com>
2023-04-18 00:43:54 +0200

    Static version - nav main active

--- dist/_nuxt/static/1681763951/videa/urbex-opustena-vila-pfaffenhof-a-radobyl/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1681763951",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Furbex-opustena-vila-pfaffenhof-a-radobyl",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
