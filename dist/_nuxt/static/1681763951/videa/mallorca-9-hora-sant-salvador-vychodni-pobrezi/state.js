fd392630c41bd41744f25ae7020ea45a62fe5e64
frytolnacestach <frytolnacestach@gmail.com>
2023-04-17 23:10:33 +0200

    Static version - components

---
+++ dist/_nuxt/static/1681763951/videa/mallorca-9-hora-sant-salvador-vychodni-pobrezi/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1681763951",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fmallorca-9-hora-sant-salvador-vychodni-pobrezi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
