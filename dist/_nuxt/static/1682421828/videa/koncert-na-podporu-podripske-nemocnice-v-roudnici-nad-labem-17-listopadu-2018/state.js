fd24ffb532ad031a3032e10ac41ef3e42cc7ba37
frytolnacestach <frytolnacestach@gmail.com>
2023-04-26 00:08:51 +0200

    Static version - Update for PageSpeed Insights

--- dist/_nuxt/static/1682421828/videa/koncert-na-podporu-podripske-nemocnice-v-roudnici-nad-labem-17-listopadu-2018/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1682421828",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fkoncert-na-podporu-podripske-nemocnice-v-roudnici-nad-labem-17-listopadu-2018",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
