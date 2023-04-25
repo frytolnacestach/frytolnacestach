623cebaa9aae262281ee642d760bef4220241dfd
frytolnacestach <frytolnacestach@gmail.com>
2023-04-25 15:45:01 +0200

    Static version - response images and other changes

--- dist/_nuxt/static/1681820821/svet/mesto/dusanbe/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1681820821",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsvet\u002Fmesto\u002Fdusanbe",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
