d4635494ff9fc6f59087bd82b4d19ea2d4eaa855
frytolnacestach <frytolnacestach@gmail.com>
2023-05-02 11:53:38 +0200

    Static version - Google Ads

--- dist/_nuxt/static/1682985598/videa/nejvetsi-hrackarstvi-hamleys-na-svete-vlog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1682985598",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fnejvetsi-hrackarstvi-hamleys-na-svete-vlog",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
