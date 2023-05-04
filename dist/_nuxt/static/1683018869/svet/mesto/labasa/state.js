ccab4c6c97808e8077746db76b61241ff003a4b3
frytolnacestach <frytolnacestach@gmail.com>
2023-05-04 13:51:35 +0200

    Static version - images for states

--- dist/_nuxt/static/1683018869/svet/mesto/labasa/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683018869",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsvet\u002Fmesto\u002Flabasa",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1683197158/videa/budapest-1den-nejvetsi-ruske-kolo-v-madarsku-vlog-madarsko/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683197158",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fbudapest-1den-nejvetsi-ruske-kolo-v-madarsku-vlog-madarsko",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
