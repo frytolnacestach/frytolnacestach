cfc1262826459bd3e49ab5cd05f886c1599da782
frytolnacestach <frytolnacestach@gmail.com>
2023-05-05 13:47:09 +0200

    Static version - API calls and new header and breadcrumbs in detail place

--- dist/_nuxt/static/1683197158/videa/legendy-2019-nejvetsi-vystava-aut-v-cesku-vlog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683197158",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Flegendy-2019-nejvetsi-vystava-aut-v-cesku-vlog",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1683285488/videa/ohnostroj-v-sillot-ohnostroj-na-mori-mallorca-bonus-1/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683285488",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fohnostroj-v-sillot-ohnostroj-na-mori-mallorca-bonus-1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
