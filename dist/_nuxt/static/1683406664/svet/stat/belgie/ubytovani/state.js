3402e22395cfa2d4acff6a943d9cb9f07fab5fdb
frytolnacestach <frytolnacestach@gmail.com>
2023-05-08 20:17:56 +0200

    Static version - Booking for cities

--- dist/_nuxt/static/1683406664/svet/stat/belgie/ubytovani/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683406664",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fbelgie\u002Fubytovani",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- dist/_nuxt/static/1683406664/videa/narodni-pamatnik-na-vitkove-uzasna-cesta-z-krejcarku-stary-vitkovsky-tunel/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683406664",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fnarodni-pamatnik-na-vitkove-uzasna-cesta-z-krejcarku-stary-vitkovsky-tunel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
