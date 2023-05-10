e21203b9941ecc6a5b447f0341cd9e79f6ce89a1
frytolnacestach <frytolnacestach@gmail.com>
2023-05-11 00:27:39 +0200

    Static version - bugfix export

--- dist/_nuxt/static/1683680408/videa/druha-vlna-jak-je-to-mozne-hoaxy-o-koronaviru/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683680408",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fdruha-vlna-jak-je-to-mozne-hoaxy-o-koronaviru",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
