d5383212cfc58b912e01e56386db08908e31e0e8
frytolnacestach <frytolnacestach@gmail.com>
2023-05-10 03:20:31 +0200

    Static version - Api call, city slug etc

---
+++ dist/_nuxt/static/1683680408/svet/stat/saint-pierre-a-miquelon/kontakty/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683680408",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fsaint-pierre-a-miquelon\u002Fkontakty",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1683680408/videa/co-je-koronavirus-2019-ncov-sars-cov-2-covid-19-skutecne-je-tak-nebezpecny-a-kdy-to-skonci/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1683680408",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fco-je-koronavirus-2019-ncov-sars-cov-2-covid-19-skutecne-je-tak-nebezpecny-a-kdy-to-skonci",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
