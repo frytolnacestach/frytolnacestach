003a9c7c04a0e3af18547da5bb12dea957431abe
frytolnacestach <frytolnacestach@gmail.com>
2023-05-11 18:09:16 +0200

    Cookies and conditions to db

--- pages/conditions.vue
+++ pages/conditions.vue
@@ -12,55 +12,7 @@
         <!-- SECTION - Wysiwyg -->
         <section class="t-section t-section--wysiwyg py-4">
             <div class="t-section__inner">
-                <div class="o-wysiwyg">
-                    <p>Ochranu osobních údajů bereme velmi vážně. Níže najdete veškeré informace o tom, jaké údaje máme k dispozici a jak s nimi nakládáme. Ochrana osobních údajů se řídí nařízením GDPR (formálně nařízení Evropského parlamentu Rady č. 2016/679 ze dne 27. 4. 2016 o ochraně fyzických osob v souvislosti se zpracováním osobních údajů a o volném pohybu těchto údajů a o zrušení směrnice 95/46/ES).</p>
-
-                    <h2>Kdo jsem a jak mě můžete kontaktovat:</h2>
-                    <p>
-                    Michal Fryč<br>
-                    Vrchlického 995,<br>
-                    Roudnice nad Labem<br>
-                    413 01<br>
-                    TEL: +420 722 678 090<br>
-                    IČO: 04521587<br>
-                    Fyzická osoba podnikající dle živnostenského zákona nezapsaná v obchodním rejstříku od 29. října 2015
-                    </p>
-                    
-                    <h2>Zpracováváme tyto osobní údaje:</h2>
-                    <p>Žádné</p>
-
-                    <h2>Účel a doba zpracování:</h2>
-                    <p>Zatím nic nezpracováváme pouze v rámci cookies a třetích stran</p>
-                    
-                    <h2>Zpracovatelé:</h2>
-                    <p>Abychom vám nabízeli špičkové služby, využíváme služby třetích stran. Níže najdete jejich jmenovitý seznam. Využíváme i zpracovatele mimo evropskou unii, konkrétně využíme software u USA. Předání údajů proběhne na základě ověření zpracovatele platformou Privacy Shield. (Tato platforma kontroluje, zda je americký software v souladu s evropskou legislativou.)</p>
-                    <ul>
-                        <li>Poskytovatel softwaru Google LLC.</li>
-                        <li>Poskytovatel hostingu FORPSI provozovatel INTERNET CZ, a. s.,</li>
-                        <li>Případně další poskytovatelé zpracovatelských softwarů, služeb a aplikací, které však v současné době společnost nevyužívá.</li>
-                    </ul>
-                    
-                    <h2>Cookies</h2>
-                    <p>Na webových stránkách využíváme cookies. Analytické cookies, které měří návštěvnost těchto stránek, považujeme za nás oprávněný zájem. Cookies pro cílení reklamy zpracováváme jen na základě vašeho souhlasu. Používání cookies můžete na svém počítači zakázat. Více o fungování cookies najdete zde.</p>
-
-                    <h2>Zabezpečení a ochrana osobních údajů</h2>
-                    <p>Přijímáme veškerá technická a organizační opatření k zabezpečení ochrany osobních údajů způsobem uvedeným v Zákoně nebo v jiných závazných právních předpisech a k vyloučení možnosti neoprávněného nebo nahodilého přístupu k osobním údajům, k jejich změně, zničení či ztrátě, neoprávněným přenosům, k jejich jinému neoprávněnému zpracování, jakož i k jinému zneužití osobních údajů. Tato povinnost platí i po ukončení zpracování osobních údajů.</p>
-
-                    <p>Veškerá elektronická data uložená chráníme zabezpečeným protokolem (HTTPS šifrovaný pomocí TLS), který chrání spojení vašeho webového prohlížeče a našeho serveru před odposloucháváním a podvrhy dat.</p>
-
-                    <h2>Vaše práva:</h2>
-                    <p>Na základě nařízení GDPR máte tato práva:</p>
-                    <ul>
-                        <li>vzít souhlas kdykoli zpět;</li>
-                        <li>požadovat po nás informace o zpracování osobních údajů;</li>
-                        <li>požadovat po nás přístup k osobním údajům;</li>
-                        <li>požadovat po nás opravu nepřesných osobních údajů, jejich doplnění či aktualizaci;</li>
-                        <li>požadovat po nás bezodkladný výmaz osobních údajů;</li>
-                        <li>požadovat po nás omezení zpracování osobních údajů;</li>
-                        <li>požádat nás o přenos osobních údajů ve strojově čitelném formátu;</li>
-                        <li>v případě pochybností o zákonnost zpracování vašich osobních údajů máte právo obrátit se na Úřad pro ochranu osobních údajů.</li>
-                    </ul>
-                </div>
+                <oWysiwyg :text="base[0].conditions" />
             </div>
         </section>
         <!-- SECTION - Wysiwyg END -->
@@ -70,17 +22,20 @@
 
 <script>
     import oHero from '../components/organisms/oHero.vue'
+    import oWysiwyg from '~/components/organisms/oWysiwyg.vue'
 
     export default {
         name: 'PageConditions',
 
         components: {
-            oHero
+            oHero,
+            oWysiwyg
         },
 
         data() {
             return {
-                headline: "Obchodní podmínky a zásady ochrany osobních údajů"
+                headline: "Obchodní podmínky a zásady ochrany osobních údajů",
+                base: this.base
             }
         },
 
@@ -95,6 +50,10 @@
                 { hid: 'og:url', content: `${process.env.baseUrl}/conditions` },
                 { hid: 'og:type', content: 'website' }  
             ]
+        },
+
+        async fetch() {
+            this.base = await fetch("https://frytolnacestach-api.vercel.app/api/base").then((res) => res.json());
         }
     }
 </script>
--- pages/cookies.vue
+++ pages/cookies.vue
@@ -12,73 +12,7 @@
         <!-- SECTION - Wysiwyg -->
         <section class="t-section t-section--wysiwyg py-4">
             <div class="t-section__inner">
-                <div class="o-wysiwyg">
-                    <p>Na webu frytolnacestach.cz jsou v souladu se zákony používány cookies</p>
-                    
-                    <h2>Co je to cookies?</h2>
-                    <p>Jako cookie (anglicky koláček, oplatka, sušenka) se v protokolu HTTP označuje malé množství dat, která WWW server pošle prohlížeči, který je uloží na počítači uživatele. Při každé další návštěvě téhož serveru pak prohlížeč tato data posílá zpět serveru. Cookies běžně slouží k rozlišování jednotlivých uživatelů, ukládají se do nich uživatelské předvolby apod., a zjednodušují tak používání webů.</p>
-
-                    <p>Cookies se ukládají na straně klienta (uživatele) jako krátké textové soubory.</p>
-
-                    <p>Cookies neznamenají žádné nebezpečí pro počítač jako takový. Přesto mají cookies význam pro ochranu soukromí. Navštívený web si totiž může ukládat do cookies jakékoliv informace, které o návštěvníkovi zjistí, a může tak postupně zjišťovat zájmy konkrétního návštěvníka. V žádném případě však nelze cookies použít pro zjištění totožnosti návštěvníků.</p>
-
-                    <p>Informací z cookies se dá posléze využívat pro cílenou reklamu, statistické vyhodnocování chování návštěvníků apod. Tyto informace však lze získávat i bez cookies, proto toto jejich využití nemůže být považováno za zvlášť nebezpečné.</p>
-
-                    <i>Volně čerpáno z <a href="https://cs.wikipedia.org/wiki/HTTP_cookie" target="_blank">Wikipedie</a>.</i>
-
-                    <h2>Jak cookies používáme?</h2>
-                    <h3>Naše vlastní cookies</h3>
-                    <p>Slouží pro funkčnost webů. Může jít například o evidenci toho, jaké komentáře pod článkem uživatel viděl a jaké jsou pro něj nové nebo o udržování informací o přihlášení.</p>
-
-                    <h3>Cookies třetích stran</h3>
-                    <p>Používáme i nejrůznější služby našich partnerů, kteří rovněž používají cookies. Tyto cookies jsou řízeny třetími stranami a my nemáme přístup ke čtení nebo zápisu těchto dat.</p>
-
-                    <p>Jde zejména o systémy pro doručování reklamy nebo analýzy návštěvnosti.</p>
-
-                    <span>Našimi partnery jsou:</span>
-                    <ul>
-                        <li>
-                            Google Analytics – podrobnosti o tomto nástoji najdete <a href="https://www.google.com/intl/cs_ALL/analytics/index.html" target="_blank">zde</a>
-                        </li>
-                        <li>
-                            Google – bližší informace o pravidlech společnosti najdete <a href="https://policies.google.com/technologies/ads?hl=cs-cz" target="_blank">zde</a>
-                        </li>
-                        <li>
-                            Sklik – bližší informace o pravidlech systému najdete <a href="https://www.sklik.cz/" target="_blank">zde</a>
-                        </li>
-                        <li>
-                            Facebook – bližší informace o pravidlech systému najdete <a href="https://www.facebook.com/about/privacy" target="_blank">zde</a>
-                        </li>
-                    </ul>
-
-                    <p>Z používání určitých cookies se můžete odhlásit na výše uvedených stránkách nebo na adrese <a href="http://www.youronlinechoices.com/cz/vase-volby" target="_blank">http://www.youronlinechoices.com/cz/vase-volby</a></p>
-
-                    <h3>Souhlas s ukládáním cookies</h3>
-                    <p>Většina prohlížečů cookies automaticky akceptuje, pokud není prohlížeč nastaven jinak. Používáním našich webů souhlasíte s ukládáním souborů cookies.</p>
-
-                    <p>Použití cookies můžete omezit nebo zablokovat v nastavení svého webového prohlížeče.</p>
-
-                    <h3>Jak zkontrolovat nastavení cookies v různých prohlížečích</h3>
-                    <p>Informace o nastavení konkrétního prohlížeče naleznete na těchto uvedených adresách:</p>
-                    
-                    <ul>
-                        <li>
-                            <a href="https://support.google.com/chrome/answer/95647?co=GENIE.Platform%3DDesktop&hl=cs" target="_blank">Chrome</a>
-                        </li>
-                        <li>
-                            <a href="https://support.microsoft.com/cs-cz/windows/microsoft-edge-data-o-proch%C3%A1zen%C3%AD-a-ochrana-soukrom%C3%AD-bb8174ba-9d73-dcf2-9b4a-c582b4e640dd" target="_blank">Egde</a>
-                        </li>
-                        <li>
-                            <a href="https://support.mozilla.org/cs/kb/vymazani-cookies-dat-stranek?redirectslug=vymazani-cookies&redirectlocale=cs" target="_blank">Mozilla Firefox</a>
-                        </li>
-                        <li>
-                            <a href="https://support.microsoft.com/cs-cz/windows/odstran%C4%9Bn%C3%AD-a-spr%C3%A1va-soubor%C5%AF-cookie-168dab11-0753-043d-7c16-ede5947fc64d" target="_blank">Internet Explorer</a>
-                        </li>
-                        <li>
-                            <a href="https://support.apple.com/cs-cz/guide/safari/sfri11471/mac" target="_blank">Safari</a>
-                        </li>
-                    </ul>
-                </div>
+                <oWysiwyg :text="base[0].cookies" />
             </div>
         </section>
         <!-- SECTION - Wysiwyg END -->
@@ -88,17 +22,20 @@
 
 <script>
     import oHero from '../components/organisms/oHero.vue'
+    import oWysiwyg from '~/components/organisms/oWysiwyg.vue'
 
     export default {
         name: 'PageConditions',
 
         components: {
-            oHero
+            oHero,
+            oWysiwyg
         },
 
         data() {
             return {
-                headline: "Zásady Cookies"
+                headline: "Zásady Cookies",
+                base: this.base
             }
         },
 
@@ -113,6 +50,10 @@
                 { hid: 'og:url', content: `${process.env.baseUrl}/cookies` },
                 { hid: 'og:type', content: 'website' }
             ]
+        },
+
+        async fetch() {
+            this.base = await fetch("https://frytolnacestach-api.vercel.app/api/base").then((res) => res.json());
         }
     }
 </script>
