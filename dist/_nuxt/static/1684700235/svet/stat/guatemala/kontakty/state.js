60910aff20a633bfb1612d68719a4901b7670b26
frytolnacestach <frytolnacestach@gmail.com>
2023-05-22 07:24:34 +0200

    Static version - logotype and PWA

---
+++ dist/_nuxt/static/1684700235/svet/stat/guatemala/kontakty/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1684700235",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fguatemala\u002Fkontakty",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
