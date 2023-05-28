0733188474e004a0874ea8011a3a40af61112fac
frytolnacestach <frytolnacestach@gmail.com>
2023-05-28 22:40:08 +0200

    Slash (no finish)

--- middleware/redirect-trailing-slash.js
+++ middleware/redirect-trailing-slash.js
@@ -1,7 +1,7 @@
 /*export default function ({ route, redirect }) {
-  const hasTrailingSlash = /\/$/.test(route.fullPath);
-  if (hasTrailingSlash && route.fullPath !== '/') {
-    const redirectPath = route.fullPath.replace(/\/$/, '');
-    redirect(301, redirectPath);
-  }
+    const hasTrailingSlash = /\/$/.test(route.fullPath);
+    if (hasTrailingSlash && route.fullPath !== '/') {
+        const redirectPath = route.fullPath.replace(/\/$/, '');
+        redirect(301, redirectPath);
+    }
 }*/
