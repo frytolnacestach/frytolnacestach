319ce6a1966330627a2908458968b19d1b559fa2
frytolnacestach <frytolnacestach@gmail.com>
2023-06-02 08:08:54 +0200

    Static version - BUGFIX maps and search

---
+++ dist/_nuxt/static/1685646364/videa/pamatnik-lidice-obec-ktera-prestala-existovat-zemrelo-340-lidi/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1685646364",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fpamatnik-lidice-obec-ktera-prestala-existovat-zemrelo-340-lidi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ pages/ucet/index.vue
@@ -0,0 +1,63 @@
+<template>
+    <main class="t-main -bg-person -pt-menu" role="main">
+        
+        <!-- SECTION - Hero -->
+		<section class="t-section -p0 pt-2 pb-1">
+            <div class="t-section__inner">
+                <oHero :headline="headline" />
+            </div>
+        </section>
+        <!-- SECTION - Hero END -->
+
+        <!-- SECTION - Signpost -->
+		<section class="t-section -p0 pt-2 pb-1">
+            <div class="t-section__inner">
+                <oSignpost />
+            </div>
+        </section>
+        <!-- SECTION - Signpost END -->
+
+    </main>
+</template>
+
+<script>
+
+    import aButtonFillFull from '~/components/atoms/aButtonFillFull.vue'
+    import mHeadline from '~/components/molecules/mHeadline.vue'
+    import oHero from '~/components/organisms/oHero.vue'
+    import oSignpost from '~/components/organisms/oSignpost.vue'
+
+    export default {
+        name: 'UcetIndexPage',
+        
+        components: {
+            aButtonFillFull,
+            mHeadline,
+            oHero,
+            oSignpost
+        },
+
+        data() {
+            return {
+                headline: 'Účet na Frytol na cestách',
+            }
+        },
+
+        head: {
+            title: 'ÚČET | Frytol na cestách',
+            meta: [
+                { hid: 'description', name: 'description', content: 'Účet na webu Frytol na cestách.' },
+                { name: 'keywords', content: `Cestovatelský portál, úvod, cestování, svět` },
+                { property: 'og:image', content: 'https://image.frytolnacestach.cz/storage/main/og-default.png' },
+                { hid: 'og:title', content: 'Frytol na cestách' },
+                { hid: 'og:description', content: 'Účet na webu Frytol na cestách.' },
+                { hid: 'og:url', content: `${process.env.baseUrl}` },
+                { hid: 'og:type', content: 'website' }
+            ]
+        },
+
+        async asyncData({ $axios }) {
+
+        }
+    }
+</script>
