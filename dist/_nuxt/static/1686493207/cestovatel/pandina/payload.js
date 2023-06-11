708e89c5213c45fdca1c6f508d1d6c6351e586eb
frytolnacestach <frytolnacestach@gmail.com>
2023-06-11 16:58:06 +0200

    nuxt generate - google maps load and new design

---
+++ dist/_nuxt/static/1686493207/cestovatel/pandina/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/cestovatel/pandina", (function(a){return {data:[{user:[{id:2,nickname:"Pandina",slug:"pandina",surname:a,lastname:a,status:3}],placesID:[],placesContinents:[],placesStates:[],placesCities:[],placesRegions:[],placesSpots:[],imagesAll:[]}],fetch:{},mutations:void 0}}(null)));
---
+++ dist/_nuxt/static/1686493207/svet/stat/egypt/ceny/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1686493207",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fegypt\u002Fceny",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
