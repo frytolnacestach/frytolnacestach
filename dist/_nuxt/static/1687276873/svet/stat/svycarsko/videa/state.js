63f7b35e5a8726288e6b265e8193454f4f304645
frytolnacestach <frytolnacestach@gmail.com>
2023-06-22 01:38:52 +0200

    Static version - 2023-06-22
    - Bugfix (cities filter)
    - New green color
    - API calls

--- dist/_nuxt/static/1687276873/svet/stat/svycarsko/videa/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1687276873",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fsvycarsko\u002Fvidea",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1687388665/videa/alfou-tam-odtahovkou-zpet-717km-od-domova-kolik-stoji-odtah-z-ciziny-francie-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1687388665",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Falfou-tam-odtahovkou-zpet-717km-od-domova-kolik-stoji-odtah-z-ciziny-francie-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
