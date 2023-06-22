0629030d71caaa75842f54ab2b526a3f7f75fa15
frytolnacestach <frytolnacestach@gmail.com>
2023-06-22 10:41:31 +0200

    New style for a-button-pure-icon

--- pages/svet/stat/_slug.vue
+++ pages/svet/stat/_slug.vue
@@ -9,7 +9,7 @@
         <!-- SECTION - BREADCRUMBS END -->
 
         <!-- SECTION - Buttons -->
-        <section class="t-section -px-world mt-1" v-if="isMobile">
+        <section class="t-section -px-world mt-1 -p0" v-if="isMobile">
             <div class="t-section__inner">
                 <div class="flex flex-end-all">
                     <span v-if="!showHero" @click="showHero = true" class="a-button-pure-icon -cover">Obrázek</span>
