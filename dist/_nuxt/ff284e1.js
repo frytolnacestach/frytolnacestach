63712cab965c37761cfac1ccf26f676a2a0e3a51
frytolnacestach <frytolnacestach@gmail.com>
2023-07-01 14:14:35 +0200

    Static version - 2023-07-01
    - bugfix (loading image in placeType)
    - Filter in index place

--- dist/_nuxt/ff284e1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[79],{386:function(n,t,e){"use strict";e.r(t);var r={name:"OrganismsoWysiwygComponent",props:{text:{type:Array,required:!0}}},o=e(2),component=Object(o.a)(r,(function(){var n=this;return(0,n._self._c)("div",{staticClass:"o-wysiwyg",domProps:{innerHTML:n._s(n.text)}})}),[],!1,null,null,null);t.default=component.exports}}]);
---
+++ dist/_nuxt/static/1688209319/videa/jake-je-autokino-v-cesku-autokino-redhost/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1688209319",layout:"default",error:a,serverRendered:true,routePath:"\u002Fvidea\u002Fjake-je-autokino-v-cesku-autokino-redhost",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
