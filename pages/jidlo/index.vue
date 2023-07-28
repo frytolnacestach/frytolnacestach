36c99a28bec8fe5700de9a3e4213a60b360d7063
frytolnacestach <frytolnacestach@gmail.com>
2023-07-28 18:05:02 +0200

    Loading items for Foods

--- pages/jidlo/index.vue
+++ pages/jidlo/index.vue
@@ -9,13 +9,17 @@
         </section>
         <!-- SECTION - Hero food END -->
 
-        <!-- SECTION - Food list -->
+        <!-- SECTION - cestovatelsky slovnik list -->
         <section class="t-section -p0">
             <div class="t-section__inner">
                 <oCoverItem :items="foods" :images="images" type="jidlo" />
+                <div class="flex flex-center my-4" v-if="!isLoading && !noMoreItems">
+                    <span class="a-button-fill -big -green" @click="loadMoreItems">Načíst další položky</span>
+                </div>
+                <oCoverItem :items="null" :images="null" type="jidlo" :skeleton=true v-if="isLoading" />
             </div>
         </section>
-        <!-- SECTION - Food list END -->
+        <!-- SECTION - cestovatelsky slovnik list END -->
 
     </main>
 </template>
@@ -34,8 +38,15 @@
 
         data() {
             return {
-                foods: this.foods,
-                images: this.images
+                //foods: this.foods,
+                //images: this.images,
+
+                foods: [],
+                images: [],
+                isLoading: false,
+                noMoreItems: false,
+                page: 1,
+                perPage: 20
             }
         },
 
@@ -52,21 +63,89 @@
             ]
         },
 
-        //API STATIC
-        async asyncData({ $axios }) {
-            // PAGE - Foods list
-            // Foods
-            const foods = await $axios.$get(`https://api.frytolnacestach.cz/api/foods`)
-            // Images
-            const imagesFoodsIDS = foods.map(food => food.id_image_cover).filter(id => id !== null && id !== '')
-            const images = await $axios.$get(`https://api.frytolnacestach.cz/api/images-array?id=${imagesFoodsIDS.join(',')}`)
+        async mounted() {
+            await this.loadItems();
+            this.addScrollListener();
+        },
 
-            
-            //return
-            return {
-                foods,
-                images
-            }
+        methods:{
+            async loadItems() {
+                //start loading
+                this.isLoading = true;
+
+                //load foods
+                const [foodsResponse] = await Promise.all([
+                    this.$axios.get(`https://api.frytolnacestach.cz/api/foods?showType=list&page=${this.page}&items=${this.perPage}`)
+                ]);
+                const { data: foodsData } = foodsResponse;
+
+                //load images
+                const imagesfoodsIDS = foodsData.map(placeSpot => placeSpot.id_image_cover).filter(id => id !== undefined && id !== null && id !== '');
+                if (imagesfoodsIDS.length > 0) {
+                    const imagesResponse = await this.$axios.get(`https://api.frytolnacestach.cz/api/images-array?id=${imagesfoodsIDS.join(',')}`);
+                    const { data: imagesData } = imagesResponse;
+                    this.images = this.images.concat(imagesData);
+                
+                    // add to placecesData to foods
+                    this.foods = this.foods.concat(foodsData);
+                } else {
+                    // add to placecesData to foods
+                    this.foods = this.foods.concat(foodsData);
+                } 
+
+                //no more items?
+                if (foodsData.length === 0 || foodsData.length < this.perPage) {
+                    this.noMoreItems = true;
+                }
+
+                //end loading
+                this.isLoading = false;
+            },
+
+            addScrollListener() {
+                window.addEventListener('scroll', this.handleScroll);
+            },
+
+            removeScrollListener() {
+                window.removeEventListener('scroll', this.handleScroll);
+            },
+
+            loadMoreItems() {
+                //no further loading can occur while loading
+                if (this.isLoading || this.noMoreItems) {
+                    return;
+                }
+                // loading more items
+                this.page++;
+                this.loadItems();
+            },
+
+            handleScroll() {
+                //no further loading can occur while loading
+                if (this.isLoading || this.noMoreItems) {
+                    return;
+                }
+
+                // Document for scroll point
+                const windowHeight = window.innerHeight;
+                const documentHeight = document.documentElement.scrollHeight;
+                const scrollTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+                // Footer height
+                const tFooterElement = document.querySelector('.t-footer');
+                const tFooterHeight = tFooterElement.offsetHeight;
+
+                // Point for loading
+                if (scrollTop + windowHeight >= documentHeight - tFooterHeight) {
+                    // loading more items
+                    this.page++;
+                    this.loadItems();
+                }
+            },
+        },
+
+        beforeDestroy() {
+            this.removeScrollListener();
         }
     }
 </script>
