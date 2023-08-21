a92192fa860daccb557fa5af8dbfe17887356d9d
frytolnacestach <frytolnacestach@gmail.com>
2023-08-22 00:49:53 +0200

    API conect for achievements and modify class for item

--- components/organisms/oAchievements.vue
+++ components/organisms/oAchievements.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="o-achievements">
+    <div class="o-achievements" v-if="achievements[0]">
         <div class="o-achievements__outer">
             <div class="o-achievements__inner">
                 <div class="o-achievements__items">
                     
-                    <div class="o-achievements__item -type-world -value-bronze">
+                    <div class="o-achievements__item -type-world" :class="achievements[0].state >= 10 ? ' -value-bronze' : ''">
                         <div class="o-achievements__content">
                             <div class="o-achievements__medal"></div>
                             <div class="o-achievements__text">
@@ -12,7 +12,7 @@
                                 <p class="o-achievements__description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
                             </div>
                             <div class="o-achievements__progress">
-                                <span class="o-achievements__progress-number"><span class="o-achievements__progress-number-actual">10</span>/<span class="o-achievements__progress-number-need">10</span></span>
+                                <span class="o-achievements__progress-number"><span class="o-achievements__progress-number-actual">{{achievements[0].state > 10 ? 10 : achievements[0].state}}</span>/<span class="o-achievements__progress-number-need">10</span></span>
                                 <div class="o-achievements__progress-line">
                                     <div class="o-achievements__progress-line-full" style="width: 100%"></div>
                                 </div>
@@ -20,7 +20,7 @@
                         </div>
                     </div>
 
-                    <div class="o-achievements__item -type-world -value-silver">
+                    <div class="o-achievements__item -type-world" :class="achievements[0].state >= 25 ? ' -value-silver' : ''">
                         <div class="o-achievements__content">
                             <div class="o-achievements__medal"></div>
                             <div class="o-achievements__text">
@@ -28,7 +28,7 @@
                                 <p class="o-achievements__description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
                             </div>
                             <div class="o-achievements__progress">
-                                <span class="o-achievements__progress-number"><span class="o-achievements__progress-number-actual">16</span>/<span class="o-achievements__progress-number-need">25</span></span>
+                                <span class="o-achievements__progress-number"><span class="o-achievements__progress-number-actual">{{achievements[0].state > 25 ? 25 : achievements[0].state}}</span>/<span class="o-achievements__progress-number-need">25</span></span>
                                 <div class="o-achievements__progress-line">
                                     <div class="o-achievements__progress-line-full" style="width: 64%"></div>
                                 </div>
@@ -36,7 +36,7 @@
                         </div>
                     </div>
 
-                    <div class="o-achievements__item -type-world -value-gold">
+                    <div class="o-achievements__item -type-world" :class="achievements[0].state >= 125 ? ' -value-gold' : ''">
                         <div class="o-achievements__content">
                             <div class="o-achievements__medal"></div>
                             <div class="o-achievements__text">
@@ -44,7 +44,7 @@
                                 <p class="o-achievements__description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
                             </div>
                             <div class="o-achievements__progress">
-                                <span class="o-achievements__progress-number"><span class="o-achievements__progress-number-actual">16</span>/<span class="o-achievements__progress-number-need">125</span></span>
+                                <span class="o-achievements__progress-number"><span class="o-achievements__progress-number-actual">{{achievements[0].state > 125 ? 125 : achievements[0].state}}</span>/<span class="o-achievements__progress-number-need">125</span></span>
                                 <div class="o-achievements__progress-line">
                                     <div class="o-achievements__progress-line-full" style="width: 12.8%"></div>
                                 </div>
@@ -52,7 +52,7 @@
                         </div>
                     </div>
 
-                    <div class="o-achievements__item -type-world -value-diamond">
+                    <div class="o-achievements__item -type-world" :class="achievements[0].state >= 249 ? ' -value-diamond' : ''">
                         <div class="o-achievements__content">
                             <div class="o-achievements__medal"></div>
                             <div class="o-achievements__text">
@@ -60,23 +60,7 @@
                                 <p class="o-achievements__description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
                             </div>
                             <div class="o-achievements__progress">
-                                <span class="o-achievements__progress-number"><span class="o-achievements__progress-number-actual">16</span>/<span class="o-achievements__progress-number-need">249</span></span>
-                                <div class="o-achievements__progress-line">
-                                    <div class="o-achievements__progress-line-full" style="width: 6.4%"></div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="o-achievements__item -type-world">
-                        <div class="o-achievements__content">
-                            <div class="o-achievements__medal"></div>
-                            <div class="o-achievements__text">
-                                <h2 class="o-achievements__name">Světový cestovatel (Diamontový)</h2>
-                                <p class="o-achievements__description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-                            </div>
-                            <div class="o-achievements__progress">
-                                <span class="o-achievements__progress-number"><span class="o-achievements__progress-number-actual">16</span>/<span class="o-achievements__progress-number-need">249</span></span>
+                                <span class="o-achievements__progress-number"><span class="o-achievements__progress-number-actual">{{achievements[0].state > 249 ? 249 : achievements[0].state}}</span>/<span class="o-achievements__progress-number-need">249</span></span>
                                 <div class="o-achievements__progress-line">
                                     <div class="o-achievements__progress-line-full" style="width: 6.4%"></div>
                                 </div>
@@ -110,7 +94,7 @@
                 passwordHash: null,
                 loginCheck: false,
                 account: [],
-                followers: [],
+                achievements: [],
                 users: []
             }
         },
@@ -129,7 +113,7 @@
             while (!success) {
                 try {
                     let account = null
-                    let followers = null
+                    let achievements = null
                     let users = null
 
                     if (this.type === "account") {
@@ -140,15 +124,9 @@
 
                                 this.loginCheck = true
 
-                                // Followers
+                                // Achievements
                                 if (account !== null) {
-                                    followers = await this.$axios.$get(`https://api.frytolnacestach.cz/api/user-followers-id-user?id_user=${account[0].id}`)
-
-                                    if (followers !== null) {
-                                        // Users
-                                        const usersFollowersIDS = followers.map(follower => follower.id_follower).filter(id => id !== null && id !== '')
-                                        users = await this.$axios.$get(`https://api.frytolnacestach.cz/api/users-ids?id=${usersFollowersIDS.join(',')}`)
-                                    }
+                                    achievements = await this.$axios.$get(`https://api.frytolnacestach.cz/api/user-achievements?id_user=${account[0].id}`)
                                 }
                             } else {
                                 this.loginCheck = true
@@ -157,20 +135,14 @@
                             this.loginCheck = false
                         }
 
-                        data = { account, followers, users }
+                        data = { account, achievements, users }
                     } else {
                         if (process.client) {
-                                // Followers
-                                followers = await this.$axios.$get(`https://api.frytolnacestach.cz/api/user-followers-id-user?id_user=${this.idUser}`)
-
-                                if (followers !== null) {
-                                    // Users
-                                    const usersFollowersIDS = followers.map(follower => follower.id_follower).filter(id => id !== null && id !== '')
-                                    users = await this.$axios.$get(`https://api.frytolnacestach.cz/api/users-ids?id=${usersFollowersIDS.join(',')}`)
-                                }
+                                // achievements
+                                achievements = await this.$axios.$get(`https://api.frytolnacestach.cz/api/user-achievements?id_user=${this.idUser}`)
                         }
 
-                        data = { account, followers, users }
+                        data = { account, achievements, users }
                     }
 
                     
