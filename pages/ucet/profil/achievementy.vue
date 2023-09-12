119f6562b81b41ccfc57a09d8fd1ed6a021f5a81
frytolnacestach <frytolnacestach@gmail.com>
2023-09-12 21:47:30 +0200

    BUGFIX for print style

--- pages/ucet/profil/achievementy.vue
+++ pages/ucet/profil/achievementy.vue
@@ -72,13 +72,13 @@
         },
 
         head: {
-            title: 'Moje data | Cestovatelský portál Frytol na cestách',
+            title: 'Achievementy | Cestovatelský portál Frytol na cestách',
             meta: [
-                { hid: 'description', name: 'description', content: 'Moje data na webu Frytol na cestách.' },
+                { hid: 'description', name: 'description', content: 'Získané achievementy na webu Frytol na cestách.' },
                 { name: 'keywords', content: `Cestovatelský portál, úvod, cestování, svět` },
                 { property: 'og:image', content: 'https://image.frytolnacestach.cz/storage/main/og-default.png' },
-                { hid: 'og:title', content: 'Moje data | Cestovatelský portál Frytol na cestách' },
-                { hid: 'og:description', content: 'Moje data na webu Frytol na cestách.' },
+                { hid: 'og:title', content: 'Achievementy | Cestovatelský portál Frytol na cestách' },
+                { hid: 'og:description', content: 'Získané achievementy na webu Frytol na cestách.' },
                 { hid: 'og:url', content: `${process.env.baseUrl}` },
                 { hid: 'og:type', content: 'website' }
             ]
