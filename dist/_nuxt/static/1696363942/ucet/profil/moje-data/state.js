139212eb37a889372e34e2e745af182e15c31ecf
frytolnacestach <frytolnacestach@gmail.com>
2023-10-03 23:28:40 +0200

    Static version - 2023-10-03

---
+++ dist/_nuxt/static/1696363942/ucet/profil/moje-data/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1696363942",layout:"default",error:a,serverRendered:true,routePath:"\u002Fucet\u002Fprofil\u002Fmoje-data",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
