d6d40ea1d7197fa82d14d3ca40b7e3260afd1ec6
frytolnacestach <frytolnacestach@gmail.com>
2023-10-28 19:37:34 +0200

    Create skeleton for o-follower-list

---
+++ components/skeleton/skeletonoFollowerList.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="skeleton-o-follower-list">
+        <div class="skeleton-o-follower-list__outer">
+            <div class="skeleton-o-follower-list__items">
+                <div v-for="index in 5" class="skeleton-o-follower-list__item">
+                    <div class="skeleton-o-follower-list__content">
+                        <div :class="`skeleton-o-follower-list__image loading-image`+ styleThema"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'OrganismsSkeletonoFollowerListComponent',
+
+        props: {
+            styleThema: {
+                type: String,
+                required: true
+            }
+        }
+    }
+</script>
