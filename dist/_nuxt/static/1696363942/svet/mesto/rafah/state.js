b4d74fbf9a80678edb5228b9f0e69a8ae8775975
frytolnacestach <frytolnacestach@gmail.com>
2023-10-30 23:08:13 +0100

    Static version - 2023-10-30

--- dist/_nuxt/static/1696363942/svet/mesto/rafah/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1696363942",layout:"default",error:a,serverRendered:true,routePath:"\u002Fsvet\u002Fmesto\u002Frafah",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1698698170/elektrické zásuvky/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1698698170",layout:"empty",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null}}}}("\u002Felektrick%C3%A9%20z%C3%A1suvky"));
