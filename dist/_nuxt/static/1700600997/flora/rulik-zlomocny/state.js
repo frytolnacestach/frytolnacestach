8fcec37f29a6d07a67e8d01bc77dd56674e9c7c1
frytolnacestach <frytolnacestach@gmail.com>
2023-11-22 13:12:36 +0100

    Static version - 2023-11-22

--- dist/_nuxt/static/1700600997/flora/rulik-zlomocny/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1700600997",layout:"default",error:a,state:{user:a},serverRendered:true,routePath:"\u002Fflora\u002Frulik-zlomocny",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- dist/_nuxt/static/1700600997/videa/pamatnik-lidice-obec-ktera-prestala-existovat-zemrelo-340-lidi/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1700600997",layout:"default",error:a,state:{user:a},serverRendered:true,routePath:"\u002Fvidea\u002Fpamatnik-lidice-obec-ktera-prestala-existovat-zemrelo-340-lidi",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
