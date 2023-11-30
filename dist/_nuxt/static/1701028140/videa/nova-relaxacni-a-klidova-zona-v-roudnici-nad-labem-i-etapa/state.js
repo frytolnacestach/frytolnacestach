d436201d23c755cbc6cad4662ebd6c9bfecfdd44
frytolnacestach <frytolnacestach@gmail.com>
2023-11-30 22:06:55 +0100

    Static version - 2023-11-30

--- dist/_nuxt/static/1701028140/videa/nova-relaxacni-a-klidova-zona-v-roudnici-nad-labem-i-etapa/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701028140",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fnova-relaxacni-a-klidova-zona-v-roudnici-nad-labem-i-etapa",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1701374945/svet/stat/vietnam/kontakty/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701374945",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fvietnam\u002Fkontakty",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
