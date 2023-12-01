e1f9efb2f87046c644819b8c90805d638283b3d6
frytolnacestach <frytolnacestach@gmail.com>
2023-12-02 00:53:41 +0100

    Static version - 2023-12-02

---
+++ dist/_nuxt/static/1701472483/videa/mostecke-jezero-vetsi-nez-machovo-jezero/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701472483",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fmostecke-jezero-vetsi-nez-machovo-jezero",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
