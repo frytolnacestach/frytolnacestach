380beb760b44ecb09f461ca4695fd12e9e232c5e
frytolnacestach <frytolnacestach@gmail.com>
2023-12-03 14:54:51 +0100

    Static version - 2023-12-03

--- dist/_nuxt/static/1701504560/svet/stat/antarktida/videa/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701504560",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fantarktida\u002Fvidea",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1701607999/svet/stat/jizni-sudan/cesta/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701607999",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fjizni-sudan\u002Fcesta",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
