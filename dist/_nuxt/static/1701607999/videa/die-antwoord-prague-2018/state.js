16d2808dae5008888d07451c36ded799c5b686c0
frytolnacestach <frytolnacestach@gmail.com>
2023-12-04 22:14:12 +0100

    Static version - 2023-12-04

--- dist/_nuxt/static/1701607999/videa/die-antwoord-prague-2018/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701607999",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fdie-antwoord-prague-2018",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
