3b83ec1b27d4205c423375c58753cf0dba78408a
frytolnacestach <frytolnacestach@gmail.com>
2023-12-06 22:44:30 +0100

    Static version - 2023-12-06-v2

---
+++ dist/_nuxt/static/1701896624/svet/mesto/al-hasaka/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701896624",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fmesto\u002Fal-hasaka",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
