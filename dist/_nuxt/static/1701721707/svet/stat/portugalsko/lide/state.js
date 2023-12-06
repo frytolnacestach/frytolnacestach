c8594c43b0d9a93c431bc65c598013bf52f880ca
frytolnacestach <frytolnacestach@gmail.com>
2023-12-06 16:24:09 +0100

    Static version - 2023-12-06

--- dist/_nuxt/static/1701721707/svet/stat/portugalsko/lide/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701721707",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fportugalsko\u002Flide",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1701871288/videa/koncert-na-podporu-podripske-nemocnice-v-roudnici-nad-labem-17-listopadu-2018/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1701871288",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fkoncert-na-podporu-podripske-nemocnice-v-roudnici-nad-labem-17-listopadu-2018",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
