418a695b99887956dd80cb52245aac0d350c8929
frytolnacestach <frytolnacestach@gmail.com>
2023-12-16 01:57:53 +0100

    Change background color for button

--- pages/iam.vue
+++ pages/iam.vue
@@ -23,7 +23,7 @@
                 <mHeadline title="Platformy kde jsem" styleAlign=" -left" styleGap="mb-1" />
                 <oPlatform />
                 <div class="flex flex-center mb-4">
-                    <aButtonFillFull target="internal" url="/social" text="Více informací o platformách" styleThema=" -green" />
+                    <aButtonFillFull target="internal" url="/social" text="Více informací o platformách" styleThema=" -gray" />
                 </div>
             </div>
         </section>
