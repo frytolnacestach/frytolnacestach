f49abeb1d29a2346a48251776f8028f5c45c677e
frytolnacestach <frytolnacestach@gmail.com>
2023-12-21 14:32:26 +0100

    Static version - 2023-12-21

--- dist/_nuxt/static/1702647493/videa/karlovy-vary-2den-k-snidani-becherovka-a-bagel/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1702647493",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fkarlovy-vary-2den-k-snidani-becherovka-a-bagel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
