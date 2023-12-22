7c848b0e1b37579c2beb9adfecbf3e18e65f5e5f
frytolnacestach <frytolnacestach@gmail.com>
2023-12-22 21:28:21 +0100

    o-affilate-alza

--- components/organisms/oAffilateAlza.vue
+++ components/organisms/oAffilateAlza.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="o-affilate-alza">
-        <object data="https://affil.alza.cz/bnr/70482" width="100%" height="100%"></object>
+        <div class="o-affilate-alza__outer">
+            <div class="o-affilate-alza__inner">
+                <object data="https://affil.alza.cz/bnr/70482" width="100%" height="100%"></object>
+            </div>
+        </div>
     </div>
 </template>
 
