22bc110e4f533d88601833ab40bb29292b96b901
frytolnacestach <frytolnacestach@gmail.com>
2024-01-01 03:07:13 +0100

    Copyright add year

--- components/organisms/oCopyright.vue
+++ components/organisms/oCopyright.vue
@@ -2,7 +2,7 @@
     <div class="o-copyright">
         <div class="o-copyright__outer">
             <div class="o-copyright__inner">
-                <span class="o-copyright__text">&copy; Všechna práva vyhrazena Frytol na cestách</span>
+                <span class="o-copyright__text">&copy; 2024 Frytol na cestách Všechna práva vyhrazena</span>
             </div>
         </div>
     </div>
