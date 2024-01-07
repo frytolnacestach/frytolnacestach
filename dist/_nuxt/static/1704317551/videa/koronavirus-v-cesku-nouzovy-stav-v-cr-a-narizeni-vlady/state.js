544eff6636a9e6d5a308870abc62187a579e13f5
frytolnacestach <frytolnacestach@gmail.com>
2024-01-07 20:17:02 +0100

    STATIC VERSION - 2024-01-07

--- dist/_nuxt/static/1704317551/videa/koronavirus-v-cesku-nouzovy-stav-v-cr-a-narizeni-vlady/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1704317551",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fkoronavirus-v-cesku-nouzovy-stav-v-cr-a-narizeni-vlady",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
