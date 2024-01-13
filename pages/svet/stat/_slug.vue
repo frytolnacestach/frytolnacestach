130d82b478d9e686b812223ef242a63f2e0cd448
frytolnacestach <frytolnacestach@gmail.com>
2024-01-13 21:02:32 +0100

    Add tab in state (Chains)

--- pages/svet/stat/_slug.vue
+++ pages/svet/stat/_slug.vue
@@ -130,9 +130,9 @@
                                 <oCoverTitleItemState type="elektricka-zasuvka" title="Elektrické zásuvky použivané ve státě" perex="Poskytujeme vám informace pro pohodlné připojení a dobíjení vašich zařízení během pobytu, abyste byli připraveni na místní elektrickou infrastrukturu." :placeStateName="place[0].name" :placeStateID="place[0].id" v-if="place[0].id" />
                                 <!-- SECTION - Elektrické zásuvky END -->
 
-                                <!-- SECTION - Flora list -->
+                                <!-- SECTION - Řetezce list -->
                                 <oCoverItemState type="retezec" title="Řetězce ve státě" perex="Představujeme vám přehled nadnárodních i místních řetězců, abyste mohli plánovat své nákupy a stravování s lehkostí a přizpůsobit je svým preferencím." :placeStateName="place[0].name" :placeStateID="place[0].id" v-if="place[0].id" />
-                                <!-- SECTION - Flora list END -->
+                                <!-- SECTION - Řetezce list END -->
 
                                 <!-- SECTION - Review -->
                                 <oReviewItem :account="account" :IDplace="place[0].id" type="state" v-if="place[0].id" />
@@ -785,6 +785,67 @@
                 </section>
                 <!-- SECTION END -->
             </template>
+            <template v-else-if="activeTab === 'retezce'">
+                <!-- SECTION -->
+                <section class="t-section -p0">
+                    <div class="t-section__inner">
+                        <div class="t-grid -world-ful">
+                            <div class="t-grid__section -content">
+                                <!-- SECTION -->
+                                <section class="t-section -px-world -p0 mb-4">
+                                    <div class="t-section__inner">
+                                        <div class="t-grid -place-main-with-aside">
+                                            <div class="t-grid__section -main">
+                                                <!-- SECTION - Řetezce list -->
+                                                <section class="t-section -p0 -px-world my-2">
+                                                    <div class="t-section__inner">
+                                                        <oCoverItemState type="retezec" title="Řetězce ve státě" perex="Představujeme vám přehled nadnárodních i místních řetězců, abyste mohli plánovat své nákupy a stravování s lehkostí a přizpůsobit je svým preferencím." :placeStateName="place[0].name" :placeStateID="place[0].id" v-if="place[0].id" />
+                                                    </div>
+                                                </section>
+                                                <!-- SECTION - Řetezce list END -->
+                                            </div>
+
+                                            <div class="t-grid__section -aside-place-status">
+                                                <!-- SECTION - Visited button - sidebar -->
+                                                <section class="t-section -px-world my-1">
+                                                    <div class="t-section__inner">
+                                                        <oVisitedButton :account="account" :place="this.place[0].id" placeType="state" />
+                                                    </div>
+                                                </section>
+                                                <!-- SECTION - Visited button - sidebar - END -->
+                                            </div>
+                                            <div class="t-grid__section -aside-content">
+                                                <!-- SECTION - Account banner - sidebar -->
+                                                <section class="t-section -px-world my-1" v-if="account && account.length === 0">
+                                                    <div class="t-section__inner">
+                                                        <oAccountBanner :account="account" styleThema=" -green" />
+                                                    </div>
+                                                </section>
+                                                <!-- SECTION - Account banner - sidebar END -->
+
+                                                <!-- SECTION - Events - sidebar -->
+                                                <oSidebarList :place="this.place[0].id" type="state" />
+                                                <!-- SECTION - Events - sidebar - END -->
+                                            </div>
+                                            <div class="t-grid__section -aside-ad">
+                                                <!-- SECTION - ad-google - sidebar -->
+                                                <section class="t-section -px-world mt-4 mb-2">
+                                                    <div class="t-section__inner">
+                                                        <oAdGoogleSidebar styleThema=" -green" />
+                                                    </div>
+                                                </section>
+                                                <!-- SECTION - ad-google - sidebar - END -->
+                                            </div>
+                                        </div>
+                                    </div>
+                                </section>
+                                <!-- SECTION END -->
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!-- SECTION END -->
+            </template>
         </div>
     </main>
 </template>
@@ -903,7 +964,8 @@
                     { slug: 'ubytovani', label: 'Ubytování', visible: false },
                     { slug: 'videa', label: 'Videa', visible: false },
                     { slug: 'sousedni-staty', label: 'Sousední státy', visible: false },
-                    { slug: 'elektricke-zasuvky', label: 'Elektrické zásuvky', visible: false }
+                    { slug: 'elektricke-zasuvky', label: 'Elektrické zásuvky', visible: false },
+                    { slug: 'retezce', label: 'Řetězce', visible: false }
                 ],
                 mNavBreadcrumbsPlaceArray: [
                     {
@@ -1001,7 +1063,9 @@
                     this.preTitle = `${this.activeTabName} státu`
                 } else if (this.activeTab === 'elektricke-zasuvky') {
                     this.preTitle = `${this.activeTabName} použivané ve státě`
-                } else {
+                } else if (this.activeTab === 'retezce') {
+                    this.preTitle = `${this.activeTabName} ve státě`
+                }  else {
                     this.preTitle = `${this.activeTabName} ve státě`
                 }
             },
@@ -1116,6 +1180,7 @@
                 const hasTabVideos = this.place[0] && (!!this.videos && this.videos.length !== 0)
                 const hasTabNeighboring = this.place[0].ids_neighboring_countries && this.place[0].ids_neighboring_countries.length !== 0
                 const hasTabWallSockets = !!this.place[0]
+                const hasTabChains = !!this.place[0]
 
                 const newTabs = [
                     { slug: 'default', label: 'Výchozí', visible: hasTabDefault },
@@ -1127,7 +1192,8 @@
                     { slug: 'ubytovani', label: 'Ubytování', visible: hasTabHotel },
                     { slug: 'videa', label: 'Videa', visible: hasTabVideos },
                     { slug: 'sousedni-staty', label: 'Sousední státy', visible: hasTabNeighboring },
-                    { slug: 'elektricke-zasuvky', label: 'Elektrické zásuvky', visible: hasTabWallSockets }
+                    { slug: 'elektricke-zasuvky', label: 'Elektrické zásuvky', visible: hasTabWallSockets },
+                    { slug: 'retezce', label: 'Řetězce', visible: hasTabChains }
                 ]
 
                 this.tabs = newTabs
@@ -1175,6 +1241,8 @@
                     tabTitle = `${label} státu ${placeName} | Cestovatelský portál Frytol na cestách`
                 } else if (this.activeTab === 'elektricke-zasuvky') {
                     tabTitle = `${label} použivané ve státě ${placeName} | Cestovatelský portál Frytol na cestách`
+                } else if (this.activeTab === 'retezce') {
+                    tabTitle = `${label} ve státě ${placeName} | Cestovatelský portál Frytol na cestách`
                 } else {
                     tabTitle = `${label} ve státě ${placeName} | Cestovatelský portál Frytol na cestách`
                 }
