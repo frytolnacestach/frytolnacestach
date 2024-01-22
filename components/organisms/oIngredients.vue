04a856e5f5d95ea7fd4d0c2a04cba0d1c5e7a017
frytolnacestach <frytolnacestach@gmail.com>
2024-01-22 23:22:10 +0100

    Component oIngredients

---
+++ components/organisms/oIngredients.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="o-ingredients">
+        <div class="o-ingredients__outer">
+            <div class="o-ingredients__inner">
+                <div class="o-ingredients__items">
+                    <div class="o-ingredients__item" v-for="item in items" :key="item.name">
+                        <span class="o-ingredients__amount">
+                            <span class="o-ingredients__value">{{ item.value }}</span>
+                            <span class="o-ingredients__unit">{{ item.unit }}</span>
+                        </span>
+                        <span class="o-ingredients__name">{{ item.name }}</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'OrganismsoIngredientsComponent',
+
+        props: {
+            items: {
+                type: Array,
+                required: true
+            }
+        }
+    }
+</script>
