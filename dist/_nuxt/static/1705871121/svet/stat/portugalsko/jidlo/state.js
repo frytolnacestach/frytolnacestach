ed8d5524fbcce0a847a989759f44100f9ee2f013
frytolnacestach <frytolnacestach@gmail.com>
2024-01-25 00:18:33 +0100

    STATIC VERSION - 2024-01-25

--- dist/_nuxt/static/1705871121/svet/stat/portugalsko/jidlo/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1705871121",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fportugalsko\u002Fjidlo",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1706132930/videa/radovic-u-velvar-nejkrasnejsi-pozorovatelna/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1706132930",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fradovic-u-velvar-nejkrasnejsi-pozorovatelna",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
