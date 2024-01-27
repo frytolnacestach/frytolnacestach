7c5b52f06868f150ee9010b0af74f5b15493dbe0
frytolnacestach <frytolnacestach@gmail.com>
2024-01-27 23:59:09 +0100

    STATIC VERSION - 2024-01-27 - v.2

---
+++ dist/_nuxt/static/1706390832/svet/mesto/noord/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1706390832",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fmesto\u002Fnoord",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
