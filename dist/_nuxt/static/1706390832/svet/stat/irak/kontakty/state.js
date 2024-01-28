c9ff8e6abe7ad78080a772b59c4945bdd3416afa
frytolnacestach <frytolnacestach@gmail.com>
2024-01-28 13:30:11 +0100

    STATIC VERSION - 2024-01-28

--- dist/_nuxt/static/1706390832/svet/stat/irak/kontakty/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1706390832",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Firak\u002Fkontakty",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
