04c1a7e5594ab34b3e6383e30168b784d0e47d17
frytolnacestach <frytolnacestach@gmail.com>
2024-01-30 21:11:04 +0100

    oCoverItem - Implementation JSON-LD

--- components/organisms/oCoverItem.vue
+++ components/organisms/oCoverItem.vue
@@ -113,6 +113,34 @@
                 type: Boolean,
                 required: false
             }
+        },
+
+        head() {
+            // Empty Array
+            if (!this.items && this.items === null) {
+                return { script: [] };
+            }
+            // Return
+            const jsonldItems = {
+                type: 'application/ld+json',
+                json: {
+                    "@context": "https://schema.org",
+                    "@type": "ItemList",
+                    "name": (this.type === 'foods' ? 'Jídla' : this.type === 'fauna' ? 'Fauna' : this.type === 'flora' ? 'Flora' : this.type === 'brands' ? 'Výrobky' : this.type === 'chains' ? 'Řetězce' : this.type === 'chains' ? 'Řetězce' : ''),
+                    "itemListElement": this.items.map((item, index) => {
+                        return {
+                            "@type": "ListItem",
+                            "position": index + 1,
+                            "item": {
+                                "@id": 'https://frytolnacestach.cz' + `/svet/${this.type}/${item.slug}`,
+                                "name": item.name
+                            }
+                        }
+                    })
+                }
+            }
+
+            return { script: [jsonldItems] }
         }
     }
 </script>
