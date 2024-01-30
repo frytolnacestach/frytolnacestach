4147aef5b3e7413c5a78dacd3b630cf694f36ebc
frytolnacestach <frytolnacestach@gmail.com>
2024-01-30 20:19:07 +0100

    STATIC VERSION - 2024-01-30

--- dist/_nuxt/static/1706469187/svet/stat/spojene-arabske-emiraty/retezce/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1706469187",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fspojene-arabske-emiraty\u002Fretezce",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
