acba8512d0c6321daee0946aeab563f68f1b74fb
frytolnacestach <frytolnacestach@gmail.com>
2024-01-31 17:58:52 +0100

    event (page) - Implementation JSON-LD

--- pages/videa/_slug.vue
+++ pages/videa/_slug.vue
@@ -155,7 +155,7 @@
             // ogType
             ogType = 'website'
 
-            // scripts
+            // script
             let jsonldVideo
             if (this.video && this.video.length > 0) {
                 jsonldVideo = {
