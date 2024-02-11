24251fa0dbb0e3ed6b38d5ef0c197b74bdb67f4d
frytolnacestach <frytolnacestach@gmail.com>
2024-02-11 01:19:30 +0100

    STATIC VERSION - 2024-02-11

--- dist/_nuxt/static/1707250602/videa/josefinske-slavnosti-2018-v-terezine/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1707250602",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fjosefinske-slavnosti-2018-v-terezine",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- dist/_nuxt/static/1707250602/videa/prace-v-zahranici-jaky-je-zivot-na-mallorce-martin-nechvatal-podcast-1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1707250602",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fprace-v-zahranici-jaky-je-zivot-na-mallorce-martin-nechvatal-podcast-1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
