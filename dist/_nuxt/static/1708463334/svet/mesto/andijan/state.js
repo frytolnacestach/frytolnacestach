bb89fbc21d955cb913bb33c370f98035e9ca63a7
frytolnacestach <frytolnacestach@gmail.com>
2024-02-20 23:45:46 +0100

    STATIC VERSION - 2024-02-20

---
+++ dist/_nuxt/static/1708463334/svet/mesto/andijan/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1708463334",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fmesto\u002Fandijan",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
