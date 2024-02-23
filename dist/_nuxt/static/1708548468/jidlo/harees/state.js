c0ff63fb05799759187124c3209311cc5cdc5bd7
frytolnacestach <frytolnacestach@gmail.com>
2024-02-23 02:00:19 +0100

    STATIC VERSION - 2024-02-23

--- dist/_nuxt/static/1708548468/jidlo/harees/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1708548468",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fjidlo\u002Fharees",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
