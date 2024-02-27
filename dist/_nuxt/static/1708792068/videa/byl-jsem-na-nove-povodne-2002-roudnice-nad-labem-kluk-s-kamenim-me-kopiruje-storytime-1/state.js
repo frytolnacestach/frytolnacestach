52fe87964d4a29ebb53123bb94a6672fbb2c758d
frytolnacestach <frytolnacestach@gmail.com>
2024-02-27 06:35:00 +0100

    STATIC VERSION - 2024-02-27

--- dist/_nuxt/static/1708792068/videa/byl-jsem-na-nove-povodne-2002-roudnice-nad-labem-kluk-s-kamenim-me-kopiruje-storytime-1/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1708792068",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fbyl-jsem-na-nove-povodne-2002-roudnice-nad-labem-kluk-s-kamenim-me-kopiruje-storytime-1",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
