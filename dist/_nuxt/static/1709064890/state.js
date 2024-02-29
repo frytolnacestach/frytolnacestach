7e85b3cb06913d76615034a9cbc10c7875c3ff63
frytolnacestach <frytolnacestach@gmail.com>
2024-02-29 23:15:53 +0100

    STATIC VERSION - 2024-02-29

--- dist/_nuxt/static/1709064890/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1709064890",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
--- dist/_nuxt/static/1709064890/videa/narodni-pamatnik-na-vitkove-uzasna-cesta-z-krejcarku-stary-vitkovsky-tunel/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1709064890",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fnarodni-pamatnik-na-vitkove-uzasna-cesta-z-krejcarku-stary-vitkovsky-tunel",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
