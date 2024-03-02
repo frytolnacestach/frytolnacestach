0a71b4d9861c7a59068ffdf505f86b3e3017bc20
frytolnacestach <frytolnacestach@gmail.com>
2024-03-02 01:35:48 +0100

    Use setting_author_name in mAuthor

--- components/molecules/mAuthor.vue
+++ components/molecules/mAuthor.vue
@@ -1,5 +1,12 @@
 <template>
-    <i class="m-author">zdroj. <nuxtLink class="m-author__link" :to="`/cestovatel/${user[0].slug}`" v-if="user[0]">{{ user[0].nickname }}</nuxtLink></i>
+    <i class="m-author">
+        <nuxtLink class="m-author__link" :to="`/cestovatel/${user[0].slug}`" v-if="user[0]">
+            {{ user[0].surname && (user[0].setting_author_name === 1 || user[0].setting_author_name === 2) ? (user[0].surname + ' ') : '' }}
+            {{ user[0].lastname && (user[0].setting_author_name === 1 || user[0].setting_author_name === 2) ? (user[0].lastname + ' ') : '' }}
+            {{ user[0].nickname && user[0].setting_author_name === 1 ? ('(' + user[0].nickname + ')') : '' }}
+            {{ user[0].nickname && user[0].setting_author_name === 3 ? user[0].nickname : '' }}
+        </nuxtLink>
+    </i>
 </template>
 
 <script>
