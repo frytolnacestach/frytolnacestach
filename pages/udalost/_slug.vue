22b3f09b860204eacf49943e6b6e7fe4ebf79222
frytolnacestach <frytolnacestach@gmail.com>
2024-03-11 19:28:17 +0100

    Add location for JSON-LD in event

--- pages/udalost/_slug.vue
+++ pages/udalost/_slug.vue
@@ -259,19 +259,11 @@
                             "name": (this.event[0].name ? this.event[0].name : ""),
                             "startDate": (this.event[0].date_start ? this.event[0].date_start : ""),
                             "endDate": (this.event[0].date_end ? this.event[0].date_end : ""),
-                            /*"location": {
+                            "location": {
                                 "@type": "Place",
-                                "name": "Místo Konání",
-                                "address": {
-                                    "@type": "PostalAddress",
-                                    "streetAddress": "Ulice 123",
-                                    "addressLocality": "Město",
-                                    "postalCode": "12345",
-                                    "addressCountry": "Česká republika"
-                                }
-                            },*/
+                                "name": (this.placeCity[0].name ? this.placeCity[0].name : "")
+                            },
                             "description": (this.event[0].description ? this.event[0].description : ""),
-                            //"url": "https://www.udalost.cz",
                             "image": ((this.image[0] && this.image[0].id) ? ("https://image.frytolnacestach.cz/storage/events/" + this.image[0].name + ".webp") : "" )
                         }
                     }
