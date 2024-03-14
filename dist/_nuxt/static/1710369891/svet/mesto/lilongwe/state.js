d17437d1e23a52924d5892be6013b8850260f19b
frytolnacestach <frytolnacestach@gmail.com>
2024-03-15 00:32:03 +0100

    STATIC VERSION - 2024-03-15

--- dist/_nuxt/static/1710369891/svet/mesto/lilongwe/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1710369891",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fmesto\u002Flilongwe",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
