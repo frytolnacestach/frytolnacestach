496b63eee84df40f918812d17813b768e3acc9f8
frytolnacestach <frytolnacestach@gmail.com>
2024-03-16 02:35:40 +0100

    STATIC VERSION - 2024-03-16

--- dist/_nuxt/static/1710535702/videa/pf-2021-jaky-byl-rok-2020-co-se-povedlo-a-co-ne/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1710535702",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fpf-2021-jaky-byl-rok-2020-co-se-povedlo-a-co-ne",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
