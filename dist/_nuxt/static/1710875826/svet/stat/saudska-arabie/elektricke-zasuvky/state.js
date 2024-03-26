2948c794e6d5c5c36ee36decfc4469bf5d06bd66
frytolnacestach <frytolnacestach@gmail.com>
2024-03-26 19:49:24 +0100

    STATIC VERSION - 2024-03-26

--- dist/_nuxt/static/1710875826/svet/stat/saudska-arabie/elektricke-zasuvky/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1710875826",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fsaudska-arabie\u002Felektricke-zasuvky",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- dist/_nuxt/static/1710875826/videa/brozanska-pout-2019/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1710875826",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fbrozanska-pout-2019",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
