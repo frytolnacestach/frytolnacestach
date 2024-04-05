29f1a02d9b836f2c046de000c97331a1391200ba
frytolnacestach <frytolnacestach@gmail.com>
2024-04-05 12:32:13 +0200

    STATIC VERSION - 2024-04-05

--- dist/_nuxt/static/1712003433/svet/stat/angola/cesta/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712003433",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fangola\u002Fcesta",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
