2ed917aa59764ff7b4a452e3d67db612660ab33b
frytolnacestach <frytolnacestach@gmail.com>
2024-04-07 01:25:24 +0200

    STATIC VERSION - 2024-04-07

--- dist/_nuxt/static/1712308313/svet/stat/tadzikistan/lide/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712308313",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Ftadzikistan\u002Flide",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1712441082/svet/stat/japonsko/clanky/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712441082",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Fjaponsko\u002Fclanky",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
