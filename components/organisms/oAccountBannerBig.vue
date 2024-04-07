3f15133e63ba2373781041071af6e02de437e21e
frytolnacestach <frytolnacestach@gmail.com>
2024-04-07 06:53:58 +0200

    Change design for oAccountBannerBig

--- components/organisms/oAccountBannerBig.vue
+++ components/organisms/oAccountBannerBig.vue
@@ -8,8 +8,8 @@
                         Založ si účet a otevři dveře k nezapomenutelným dobrodružstvím. Získej výhody jako označování svých cest, sbírání cestovatelských úspěchů a psaní recenzí. Staň se součástí naší cestovatelské komunity.
                     </p>
                     <div class="o-account-banner-big__buttons">
-                        <aButtonFill target="internal" url="/ucet/registrace" text="Registace" styleThema=" -green" styleSize=" -small" />
-                        <aButtonFill target="internal" url="/ucet/prihlaseni" text="Přihlášení" styleThema=" -blue" styleSize=" -small" />
+                        <aButtonFill target="internal" url="/ucet/registrace" text="Registace" styleThema=" -green-inverse" styleSize=" -medium" />
+                        <aButtonFill target="internal" url="/ucet/prihlaseni" text="Přihlášení" styleThema=" -blue-inverse" styleSize=" -medium" />
                     </div>
                 </div>
             </div>
