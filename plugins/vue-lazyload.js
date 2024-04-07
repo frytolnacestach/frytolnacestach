c4a372b6b59964950dbaaf93884fe3e48f928414
frytolnacestach <frytolnacestach@gmail.com>
2024-04-07 06:20:18 +0200

    change hero to no-image

--- plugins/vue-lazyload.js
+++ plugins/vue-lazyload.js
@@ -3,7 +3,7 @@ import VueLazyload from 'vue-lazyload'
 
 Vue.use(VueLazyload, {
   preLoad: 1.3,
-  error: 'https://image.frytolnacestach.cz/storage/_default/hero.webp',
+  error: 'https://image.frytolnacestach.cz/storage/_default/no-image.webp',
   attempt: 1,
   loadingClass: 'lazyloading',
   loadedClass: 'lazyloaded'
