7f06cb8c236ebee2e4c32a1fc470399c64892079
frytolnacestach <frytolnacestach@gmail.com>
2024-04-08 22:31:21 +0200

    STATIC VERSION - 2024-04-08

--- dist/_nuxt/static/1712520534/videa/50-metrovym-prosklenym-vytahem-na-kopec-a-tramvaji-do-saskeho-svycarska-bad-schandau/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1712520534",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002F50-metrovym-prosklenym-vytahem-na-kopec-a-tramvaji-do-saskeho-svycarska-bad-schandau",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
