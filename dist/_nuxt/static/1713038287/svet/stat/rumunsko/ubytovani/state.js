40d6fdd1bc60f1e4346ffeddb267e5091ca2066b
frytolnacestach <frytolnacestach@gmail.com>
2024-04-14 21:56:11 +0200

    STATIC VERSION - 2024-04-14

--- dist/_nuxt/static/1713038287/svet/stat/rumunsko/ubytovani/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1713038287",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Frumunsko\u002Fubytovani",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
---
+++ dist/_nuxt/static/1713119812/videa/netflix-je-zabijak-kin-ale-nevzdavame-to-kino-sokol-v-roudnici-nad-labem-podcast-2/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1713119812",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fnetflix-je-zabijak-kin-ale-nevzdavame-to-kino-sokol-v-roudnici-nad-labem-podcast-2",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
