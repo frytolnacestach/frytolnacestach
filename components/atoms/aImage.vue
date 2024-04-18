662868fe00404bd0f572746bd05bde833f5d559b
frytolnacestach <frytolnacestach@gmail.com>
2024-04-18 16:56:18 +0200

    Create aImage component

---
+++ components/atoms/aImage.vue
@@ -0,0 +1,74 @@
+<template>
+    <img :class="(cssClassComponent + '__image-file') + (lazy ? ' lazyload-file' :'')"
+        :sizes="generateSizes()"
+        :srcset="generateSrcSet() + ',' + generateSrcSet('2x')"
+        :src="'https://image.frytolnacestach.cz/storage' + imageSource + imageName + '.webp'"
+        :alt="author"
+        v-bind="lazy ? 'v-lazy' : { 'fetchpriority': 'high' }"
+    >
+</template>
+
+<script>
+    export default {
+        name: 'AtomsaImageComponent',
+        
+        props: {
+            alt: {
+                type: String,
+                required: false
+            },
+            author: {
+                type: String,
+                required: false
+            },
+            lazy: {
+                type: Boolean,
+                required: false
+            },
+            imageSource: {
+                type: String,
+                required: true
+            },
+            imageName: {
+                type: String,
+                required: true
+            },
+            sizes: {
+                type: Array,
+                required: true
+            },
+            srcSet: {
+                type: Array,
+                required: true
+            },
+            cssClassComponent: {
+                type: String,
+                required: true
+            }
+        },
+
+        methods: {
+            generateSrcSet(type) {
+                if (type === "2x") {
+                    return this.sizes.map(size => {
+                        return `https://image.frytolnacestach.cz/storage/${this.imageSource + size.orientation + this.imageName}-${size.imageWidth*2}-2x.webp ${size.elementWidth*2}w`
+                    }).join(', ');
+                } else {
+                    return this.sizes.map(size => {
+                        return `https://image.frytolnacestach.cz/storage/${this.imageSource + size.orientation + this.imageName}-${size.imageWidth}.webp ${size.elementWidth}w`
+                    }).join(', ');
+                }
+            },
+
+            generateSizes() {
+                return this.srcSet.map((size, index) => {
+                    if ( size.mediaQueriesWidth === null ) {
+                        return `${size.elementWidth}px`;
+                    } else {
+                        return `(max-width: ${size.mediaQueriesWidth}px) ${size.elementWidth}px`;
+                    }
+                }).join(', ');
+            }
+        }
+    }
+</script>
