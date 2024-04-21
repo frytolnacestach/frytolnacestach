f302587db51a9a596b5418ffabcbdee5168cb2a1
frytolnacestach <frytolnacestach@gmail.com>
2024-04-21 23:58:04 +0200

    STATIC VERSION - 2024-04-21

--- dist/_nuxt/static/1713299089/svet/misto/london-eye/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1713299089",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fmisto\u002Flondon-eye",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- dist/_nuxt/static/1713299089/videa/planetarium-praha-simulator-jizdy-po-marsu-a-mesici-a-prohlidka-expozice-vlog/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1713299089",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fplanetarium-praha-simulator-jizdy-po-marsu-a-mesici-a-prohlidka-expozice-vlog",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
