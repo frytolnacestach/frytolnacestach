a8bf45aead817e19eddad8182304100c465e6aea
frytolnacestach <frytolnacestach@gmail.com>
2024-04-23 21:16:01 +0200

    STATIC VERSION - 2024-04-23

--- dist/_nuxt/static/1713732121/videa/budapest-1den-nejvetsi-ruske-kolo-v-madarsku-vlog-madarsko/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1713732121",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fvidea\u002Fbudapest-1den-nejvetsi-ruske-kolo-v-madarsku-vlog-madarsko",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
