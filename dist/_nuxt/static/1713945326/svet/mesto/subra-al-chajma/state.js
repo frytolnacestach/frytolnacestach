89ebae145f8f10ca5619fae5d00f96d3fead5026
frytolnacestach <frytolnacestach@gmail.com>
2024-04-24 11:21:32 +0200

    STATIC VERSION - 2024-04-24

---
+++ dist/_nuxt/static/1713945326/svet/mesto/subra-al-chajma/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1713945326",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fmesto\u002Fsubra-al-chajma",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
