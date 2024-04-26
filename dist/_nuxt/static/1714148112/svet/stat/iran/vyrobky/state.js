6de7b9fab3579779a7abb3a06741eec64ed41c0b
frytolnacestach <frytolnacestach@gmail.com>
2024-04-26 19:53:04 +0200

    STATIC VERSION - 2024-04-26

---
+++ dist/_nuxt/static/1714148112/svet/stat/iran/vyrobky/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1714148112",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fstat\u002Firan\u002Fvyrobky",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
