5e353a236005ef060357e401f0b9ede2565674ca
frytolnacestach <frytolnacestach@gmail.com>
2024-04-28 00:53:14 +0200

    Add data for oAccountBannerBig

--- components/organisms/oAccountBannerBig.vue
+++ components/organisms/oAccountBannerBig.vue
@@ -3,10 +3,8 @@
         <div class="o-account-banner-big__outer">
             <div class="o-account-banner-big__inner">
                 <div class="o-account-banner-big__content">
-                    <h3 class="o-account-banner-big__headline">Ty tu ještě nemáš účet?</h3>
-                    <p class="o-account-banner-big__description">
-                        Založ si účet a otevři dveře k nezapomenutelným dobrodružstvím. Získej výhody jako označování svých cest, sbírání cestovatelských úspěchů a psaní recenzí. Staň se součástí naší cestovatelské komunity.
-                    </p>
+                    <h3 class="o-account-banner-big__headline">{{ headline }}</h3>
+                    <p class="o-account-banner-big__description">{{ perex }}</p>
                     <div class="o-account-banner-big__buttons">
                         <aButtonFill target="internal" url="/ucet/registrace" text="Registace" styleThema=" -green-inverse" styleSize=" -medium" />
                         <aButtonFill target="internal" url="/ucet/prihlaseni" text="Přihlášení" styleThema=" -blue-inverse" styleSize=" -medium" />
@@ -31,6 +29,13 @@
             account: {
                 type: Array,
                 required: true
+            }
+        },
+
+        data() {
+            return {
+                headline: "Ty tu ještě nemáš účet?",
+                perex: "Založ si účet a otevři dveře k nezapomenutelným dobrodružstvím. Získej výhody jako označování svých cest, sbírání cestovatelských úspěchů a psaní recenzí. Staň se součástí naší cestovatelské komunity."
             }
         }
     }
