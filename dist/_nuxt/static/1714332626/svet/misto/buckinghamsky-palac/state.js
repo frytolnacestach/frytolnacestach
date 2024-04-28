bd7e2359eb90d82387e4efc9cd48c6273cfd2f1c
frytolnacestach <frytolnacestach@gmail.com>
2024-04-28 22:58:35 +0200

    STATIC VERSION - 2024-04-28

---
+++ dist/_nuxt/static/1714332626/svet/misto/buckinghamsky-palac/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1714332626",layout:"default",error:a,state:{account:[]},serverRendered:true,routePath:"\u002Fsvet\u002Fmisto\u002Fbuckinghamsky-palac",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
